305362d6035ead653570e0a47ea4833ff3654f9c
Egor Mironov <doramifasolyasi@gmail.com>
2022-02-15 20:22:33 +0400

    feat: layout of slider

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ burgerMenu.addEventListener('click', function(){
   burgerMenu.classList.toggle('change');
   headerDown.classList.toggle('active')
 })
+
+const chevronLeft = document.querySelector('.slider__chevron-left')
+const chevronRight = document.querySelector('.slider__chevron-right')
+const sliderItem = document.querySelector('.slider__item1')
+const sliderItem1 = document.querySelector('.slider__item2')
+
+chevronRight.addEventListener('click', function(){
+    sliderItem.classList.toggle('translate__item1')
+    sliderItem1.classList.toggle('block__items')
+})
+
+chevronLeft.addEventListener('click', function(){
+    sliderItem.classList.toggle('translate__item2')
+    sliderItem1.classList.toggle('block__items')
+})
